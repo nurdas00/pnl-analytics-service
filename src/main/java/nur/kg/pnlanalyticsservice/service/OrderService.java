783e7d6e61b6e5f8baf38414f903cd66627fd6db
nurdas <nurdas00@gmail.com>
2026-01-24 23:55:34 +0600

    remove todo

--- src/main/java/nur/kg/pnlanalyticsservice/service/OrderService.java
+++ src/main/java/nur/kg/pnlanalyticsservice/service/OrderService.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Service
 public class OrderService {
 
-    // TODO save order data that was placed in exchange service
     public void processOrder(@RequestBody OrderDto order){
 
     }
